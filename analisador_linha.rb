a8647afbd0db11c82fc005bc980c19bf1cef7225
Yohanna <yohannaoliveira12@gmail.com>
2021-11-02 04:49:00 -0300

    .

--- analisador_linha.rb
+++ analisador_linha.rb
@@ -1,21 +1,26 @@
 class AnalisadorLinha
+
   def initialize(n_linha, conteudo)
     @n_linha = n_linha
     @conteudo = conteudo
-    @frequencia = 0
+    @frequencia
   end
+
   def gerar_analise()
     @frequencia = @conteudo.split.each_with_object(Hash.new(0)) {
       |palavra, hash|
       hash[palavra] += 1
     }
   end
+
   def to_str
-      "#{@frequencia} \n"
+    "#{@frequencia} \n"
   end
+
 end
 
 numero_linhas = 0
+
 File.open('palavras.txt').each do |conteudo|
   analiseFinal = AnalisadorLinha.new(numero_linhas, conteudo)
   analiseFinal.gerar_analise
